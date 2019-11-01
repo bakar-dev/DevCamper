0be25376f83c7c2daf63b4242761099153fe2875
Bakr Hamid <baker.pucit@gmail.com>
2019-11-01 19:12:21 +0500

    unhandled promis rejection added to server.js

--- server.js
+++ server.js
@@ -20,7 +20,14 @@ app.use(logger);
 // Mount Routes
 app.use("/api/v1/bootcamps", bootcamps);
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`server runing on ${process.env.NODE_ENV} mode on port: ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
