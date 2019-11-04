c6c92ed065c51959467d4a385802bb7f90f8c1f1
Bakr Hamid <baker.pucit@gmail.com>
2019-11-04 12:39:27 +0500

    Colors pkg added to colorize the console + data Json added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
 const logger = require("./middleware/logger");
 const connectDB = require("./config/db");
 
@@ -22,12 +23,14 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
+  )
 );
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server & exit process
   server.close(() => process.exit(1));
 });
